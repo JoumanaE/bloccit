47533ea219739f7ee61d6fe6c952dbb16e3caefe
JoumanaE <joumana.elalaoui@gmail.com>
2015-12-19 13:22:04 -0500

    Assignment 35 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,10 @@
 class Topic < ActiveRecord::Base
+  belongs_to :user
   has_many :posts, dependent: :destroy
   has_many :SponsoredPost, dependent: :destroy
+
+  validates :name, length: {minimum: 5 },
+  validates :description, length: {minimum: 15 }
+  validates :user, presence: true
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,6 +16,14 @@ RSpec.describe Post, type: :model do
   it { should validate_length_of(:title).is_at_least(5) }
   it { should validate_length_of(:body).is_at_least(20) }
 
+  it {should belong_to(:post)}
+
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:body)}
+
+  it {should validate_length_of(:title).is_at_least(5) }
+  it {should validate_length_of(:body).is_at_least(15) }
+
   describe "attributes" do
     it "should respond to title" do
     expect(post).to respond_to(:title)
