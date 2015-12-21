db3570c7cbfdcaee8364249b6241b5bc18f9aacd
JoumanaE <joumana.elalaoui@gmail.com>
2015-12-21 15:47:39 -0500

    Assignment 36 completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,5 +43,11 @@ RSpec.describe User, type: :model do
      it "should be an invalid user due to incorrectly formatted email address" do
        expect(user_with_invalid_email_format).to_not be_valid
      end
+
+     it "should format the user name" do
+       user.name = "first second"
+       user.save
+       expect(user.name).to eq "First Second"
+     end
    end
  end
