89ea839448dde407f318cba28fce6980b69f5999
JoumanaE <joumana.elalaoui@gmail.com>
2015-12-22 11:05:42 -0500

    checkpoint-37-authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
     resources :sponsored_posts
-    resources :users, only: [:new, :create]
   end
+
+resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
   get "welcome/index"
